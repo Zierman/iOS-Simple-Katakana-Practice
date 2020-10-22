6d078ec100453feade3faa0a4daa15e0ce0a3521
Joshua Zierman <github@zierman.us>
2020-10-22 15:14:20 -0500

    Adding Files

---
+++ SimpleKatakanaPractice/SimpleKatakanaPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleKatakanaPracticeApp.swift
+//  SimpleKatakanaPractice
+//
+//  Created by Joshua Zierman on 10/22/20.
+//
+
+import SwiftUI
+
+@main
+struct SimpleKatakanaPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
